e5477c26a2e6b995ce167422da6fa9be92e146b2
njdam <jeandamngaboyimana@gmail.com>
2023-10-31 00:22:16 +0000

    4. Some stats?

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -16,13 +16,13 @@ classes = {"users": "User", "places": "Place", "states": "State",
            "reviews": "Review"}
 
 
-@app_views.route('/status', methods=['GET'])
+@app_views.route('/status')
 def status():
     """ A function to return status of API. """
     return jsonify({'status': 'OK'})
 
 
-@app_views.route('/stats', methods=['GET'])
+@app_views.route('/stats')
 def count():
     """ A function to return a number of each objects by type. """
     count_dict = {}
