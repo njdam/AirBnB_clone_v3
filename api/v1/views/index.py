c3dc1440c813c9ef1b73d4e83b3bf78271c1a70b
njdam <jeandamngaboyimana@gmail.com>
2023-10-31 00:19:45 +0000

    4. Some stats?

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,31 +1,31 @@
 #!/usr/bin/python3
 """THis is the index page view"""
 
+from api.v1.views import app_views
 from flask import jsonify
 from models import storage
-from api.v1.views import app_views
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+
+classes = {"users": "User", "places": "Place", "states": "State",
+           "cities": "City", "amenities": "Amenity",
+           "reviews": "Review"}
 
 
-@app_views.route('/status')
+@app_views.route('/status', methods=['GET'])
 def status():
     """ A function to return status of API. """
-    status = {"status": "OK"}
-    return jsonify(status)
+    return jsonify({'status': 'OK'})
 
 
-@app_views.route('/stats')
+@app_views.route('/stats', methods=['GET'])
 def count():
     """ A function to return a number of each objects by type. """
-    total = {}
-    classes = {
-            "Amenity": "amenities",
-            "City": "cities",
-            "Place": "places",
-            "Review": "reviews",
-            "State": "states",
-            "User": "users"
-            }
+    count_dict = {}
     for cls in classes:
-        count = storage.count(cls)
-        total[classes.get(cls)] = count
-    return jsonify(total)
+        count_dict[cls] = storage.count(classes[cls])
+    return jsonify(count_dict)
