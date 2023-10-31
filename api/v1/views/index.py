8df35dbb4b774a4bfed37afb6062e678c1a396ea
njdam <jeandamngaboyimana@gmail.com>
2023-10-31 00:00:06 +0000

    4. Some stats?

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,23 +1,46 @@
 #!/usr/bin/python3
 """THis is the index page view"""
-from api.v1.views import app_views
+
 from flask import jsonify
+
+from api.v1.views import app_views
 from models import storage
-from models.user import User
-from models.place import Place
-from models.state import State
-from models.city import City
 from models.amenity import Amenity
+from models.city import City
+from models.place import Place
 from models.review import Review
-
-classes = {"users": "User", "places": "Place", "states": "State",
-           "cities": "City", "amenities": "Amenity",
-           "reviews": "Review"}
+from models.state import State
+from models.user import User
 
 
+@app_views.route('/status')
+def get_status():
+    '''This is the status of the api
+    '''
+    return jsonify(status='OK')
+
+
+@app_views.route('/stats')
+def get_stats():
+    '''Gets the number of objects for each type.
+    '''
+    objects = {
+        'amenities': Amenity,
+        'cities': City,
+        'places': Place,
+        'reviews': Review,
+        'states': State,
+        'users': User
+    }
+    for key, value in objects.items():
+        objects[key] = storage.count(value)
+    return jsonify(objects)
+
+
+"""
 # For Task 3. Status of your API
 @app_views.route('/status', methods=['GET'])
-def get_status():
+def status():
     '''This is the status of the api'''
     return jsonify({'status': 'OK'})
 
@@ -30,3 +53,4 @@ def count():
     for cls in classes:
         count_dict[cls] = storage.count(classes[cls])
     return jsonify(count_dict)
+"""
