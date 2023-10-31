492805ab451c8e9adfc65728b77a2c7f4d0ac083
njdam <jeandamngaboyimana@gmail.com>
2023-10-31 01:47:00 +0000

    Updated!

--- models/user.py
+++ models/user.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """ holds class User"""
 import models
-import hashlib
 from models.base_model import BaseModel, Base
 from os import getenv
 import sqlalchemy
@@ -19,16 +18,6 @@ class User(BaseModel, Base):
         last_name = Column(String(128), nullable=True)
         places = relationship("Place", backref="user")
         reviews = relationship("Review", backref="user")
-        places = relationship(
-            "Place",
-            cascade="all, delete, delete-orphan",
-            backref="user"
-        )
-        reviews = relationship(
-            "Review",
-            cascade="all, delete, delete-orphan",
-            backref="user"
-        )
     else:
         email = ""
         password = ""
@@ -38,12 +27,3 @@ class User(BaseModel, Base):
     def __init__(self, *args, **kwargs):
         """initializes user"""
         super().__init__(*args, **kwargs)
-
-    def __setattr__(self, __name: str, __value) -> None:
-        '''Setting attribute of a class to a value.'''
-        if __name == 'password':
-            if type(__value) is str:
-                md_hash = hashlib.md5(bytes(__value, 'utf-8'))
-                super().__setattr__(__name, m_hash.hexdigest())
-        else:
-            super().__setattr__(__name, __value)
